d71e418be119b218a6af5f28e8f7543e9d13c6a0
Huanhuan Huang <huanhuan.huang@betterbills.com>
2015-11-15 22:29:37 +1100

    added file upload

--- js/client.js
+++ js/client.js
@@ -1,1 +1,12 @@
-// Edit me.
+function displayImage(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      var original = document.getElementById('original');
+      original.src = e.target.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
