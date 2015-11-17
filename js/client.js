8f1e960dd9115b2eef72973b8764fb18df26fea3
Huanhuan Huang <huanhuan.huang@betterbills.com>
2015-11-17 21:46:40 +1100

    display canvas image

--- js/client.js
+++ js/client.js
@@ -1,4 +1,5 @@
 function displayImage(input) {
+  var imageWidth, imageHeight = 0;
   if (input.files && input.files[0]) {
     var reader = new FileReader();
 
@@ -6,7 +7,24 @@ function displayImage(input) {
       var original = document.getElementById('original');
       original.src = e.target.result;
       original.style.display = "inline-block";
+      original.onload = function() {
+        imageWidth = this.width;
+        imageHeight = this.height;
+
+        var image = new Image();
+        image.src = original.src;
+
+        var canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        canvas.id = 'outputImage';
+        canvas.getContext('2d').drawImage(image, 0, 0);
+
+        document.getElementsByClassName('output')[0].replaceChild(canvas, document.getElementById('outputImage'));
+      }
     };
+
+
 
     reader.readAsDataURL(input.files[0]);
   }
@@ -14,7 +32,7 @@ function displayImage(input) {
 
 window.onload = function() {
   document.getElementById('original').style.display = 'none';
-  document.getElementById('inputOriginal').onchange = function() {
+  document.getElementById('imageInput').onchange = function () {
     displayImage(this);
   }
-}
+};
