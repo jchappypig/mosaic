6924bb008f973c0d9070a0ea35102152294549aa
Huanhuan Huang <huanhuan.huang@betterbills.com>
2015-11-18 00:02:21 +1100

    Finally, I have my mosaic

--- js/client.js
+++ js/client.js
@@ -5,6 +5,7 @@ function displayImage(input) {
 
     reader.onload = function (e) {
       imageDataUrl = e.target.result;
+      var blockSize = 4;
       var image = new Image();
       image.src = imageDataUrl;
       image.onload = function() {
@@ -26,18 +27,51 @@ function displayImage(input) {
         outputCanvas.height = imageHeight;
         var outputCanvasContext = outputCanvas.getContext('2d');
 
-        numberOfCols = Math.floor(imageWidth/TILE_WIDTH);
-        numberOfRows = Math.floor(imageHeight/TILE_HEIGHT);
+        var numberOfCols = Math.floor(imageWidth / TILE_WIDTH);
+        var numberOfRows = Math.floor(imageHeight / TILE_HEIGHT);
+
+        var drawTile = function(row, col, tileImage) {
+          return function() {
+            outputCanvasContext.drawImage(tileImage, col*TILE_WIDTH, row*TILE_HEIGHT);
+          };
+        };
 
         for(var row=0; row<numberOfRows; row++) {
           for(var col=0; col<numberOfCols; col++) {
-            outputCanvasContext.drawImage(image, col*TILE_WIDTH, row*TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, col*TILE_WIDTH, row*TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+            var tileImageData = inputCanvasContext.getImageData(col*TILE_WIDTH, row*TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT).data;
+            var i = -4;
+            var rgb = {r:0,g:0,b:0};
+            var count = 0;
+
+            while ( (i += blockSize * 4) < tileImageData.length ) {
+              ++count;
+              rgb.r += tileImageData[i];
+              rgb.g += tileImageData[i+1];
+              rgb.b += tileImageData[i+2];
+            }
+
+            rgb.r = ~~(rgb.r/count);
+            rgb.g = ~~(rgb.g/count);
+            rgb.b = ~~(rgb.b/count);
+
+            function componentToHex(c) {
+              var hex = c.toString(16);
+              return hex.length == 1 ? "0" + hex : hex;
+            }
+
+            function rgbToHex(r, g, b) {
+              return componentToHex(r) + componentToHex(g) + componentToHex(b);
+            }
+
+            var hex = rgbToHex(rgb.r, rgb.g, rgb.b);
+            var tileImage = new Image();
+            tileImage.src = '/color/' + hex;
+
+            tileImage.onload = drawTile(row, col, tileImage);
           }
         }
 
-
         document.getElementsByClassName('output')[0].replaceChild(outputCanvas, document.getElementById('outputImage'));
-
       }
     };
 
